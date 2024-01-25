e2705e3ec963c4382f32cac53063b33006e595c5
ghostb0x <ghostb0x.github@gmail.com>
2024-01-25 14:18:56 -0800

    Remove ip_address prop that was pulled from headers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
-import TimeDisplay from '@/components/TimeDisplay';
-import GetIP from '@/components/GetIP';
 import ClockContents from '@/components/ClockContents';
 
 export default function Home() {
-  const ip_address = GetIP();
 
-  return <ClockContents ip_address={ip_address} />;
+  return <ClockContents />;
 }
